2abcfbf98eed7bb752ea969074c0a7ea88888fc2
BerejMan <tbm6040@gmail.com>
2021-04-05 21:45:42 +0300

    Initial commit

---
+++ src/Factorization/Factor.java
@@ -0,0 +1,29 @@
+package Factorization;
+
+public class Factor {
+    public void FS2E(int num) {
+        int firFact = 2, secFact = 2;
+        int fin = 0;
+        while (true){
+            for(int i = 0; i < 1000000; i++) {
+                int res = firFact * secFact;
+                System.out.println(res);
+                secFact++;
+                if (res == num) {
+                    fin = res;
+                    System.out.println("The factors: " + firFact + " and " + secFact);
+                    break;
+                }
+
+            }
+            if (fin == num) break;
+            firFact++;
+            secFact = 2;
+        }
+
+    }
+    public int CS2E(int num) {
+
+        return -1;
+    }
+}
---
+++ src/Factorization/Main.java
@@ -0,0 +1,9 @@
+package Factorization;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Factor obj = new Factor();
+
+    }
+}
